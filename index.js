702fb9ddb9e50080ca408a2e4ec346b2cb0708ae
Jay-P-P <jpp0016@auburn.edu>
2019-07-24 12:37:15 -0500

    Added transition for the project section and minor refactoring for the checkForAnimationBreakpoints function

--- index.js
+++ index.js
@@ -4,21 +4,15 @@ window.onload = function(event) {
   var about = document.querySelector('.about');
   var face = document.querySelector('.face');
   var education = document.querySelector('.education');
+  var projects = document.querySelector('.projects');
 
   landingFirstName.style.width = '100%';
   setTimeout(function() {
     landingLastName.style.width = '100%';
-  }, 500);
+  }, 250);
   setTimeout(function() {
     landingTitle.style = 'opacity: 1; transform: translateY(0)';
-  }, 1000);
-
-  var animationsShown = {
-    aboutAndFace: false,
-    education: false,
-    skills: false,
-    projects: false
-  };
+  }, 500);
 
   var navbarButton = document.querySelector('.navbar-toggle');
   var menu = document.querySelector('.navbar');
@@ -68,18 +62,15 @@ window.onload = function(event) {
   });
 
   function showAboutAndFace() {
-    animationsShown['aboutAndFace'] = true;
     about.style = 'opacity: 1; transform: translateY(0);';
     face.children[0].style = 'opacity: 1; transform: translateY(0);';
   }
 
   function showEducation() {
-    animationsShown['education'] = true;
     education.style = 'opacity: 1; transform: translateY(0);';
   }
 
   function showSkills() {
-    animationsShown['skills'] = true;
     let timeout = 200;
 
     skillsHeading.style.opacity = 1;
@@ -92,30 +83,60 @@ window.onload = function(event) {
     });
   }
 
-  var lastScrollPosition = window.scrollY;
+  function showProjects() {
+    projects.style = 'transform: translateY(0);';
+    var projectsChildren = [
+      ...projects.getElementsByClassName('project-container')
+    ];
+
+    var timeout = 1000;
+    projectsChildren.forEach(function(project) {
+      setTimeout(function() {
+        project.style = 'opacity: 1; transform: scale(1);';
+      }, timeout);
+      timeout += 250;
+    });
+  }
+
+  var breakPoints = [
+    {
+      // Section: aboutAndFace
+      breakPointTrigger: landing.clientHeight / 2,
+      triggerFunction: showAboutAndFace,
+      shown: false
+    },
+    {
+      // Section: education
+      breakPointTrigger: landing.clientHeight + about.clientHeight / 2,
+      triggerFunction: showEducation,
+      shown: false
+    },
+    {
+      // Section: skills
+      breakPointTrigger: landing.clientHeight + about.clientHeight,
+      triggerFunction: showSkills,
+      shown: false
+    },
+    {
+      // Section: projects
+      breakPointTrigger:
+        landing.clientHeight + about.clientHeight + skills.clientHeight,
+      triggerFunction: showProjects,
+      shown: false
+    }
+  ];
+
+  var position = window.scrollY;
 
   function checkForAnimationBreakpoints() {
     position = window.scrollY;
-    if (
-      !animationsShown['aboutAndFace'] &&
-      position >= landing.offsetHeight / 2
-    ) {
-      showAboutAndFace();
-    }
-
-    if (
-      !animationsShown['education'] &&
-      position >= landing.offsetHeight + about.offsetHeight / 2
-    ) {
-      showEducation();
-    }
-
-    if (
-      !animationsShown['skills'] &&
-      position >= landing.offsetHeight + about.offsetHeight
-    ) {
-      showSkills();
-    }
+    breakPoints.forEach(function(point, index) {
+      if (!point.shown && position >= point.breakPointTrigger) {
+        point.shown = true;
+        point.triggerFunction();
+        breakPoints.splice(index, 1); // Remove the breakpoint
+      }
+    });
   }
 
   window.addEventListener('scroll', function() {
